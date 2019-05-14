61f9d28e267a808358ab45920266e3740a898b49
Que Master <quoeamaster@gmail.com>
2019-05-14 06:38:54 -0400

    2019-05-19a - added testing approaches on timeCron conversion {from any timezone to UTC}, pending for cron schedule actual setting

--- webservice/cronService.go
+++ webservice/cronService.go
@@ -23,6 +23,8 @@ import (
 	"github.com/daviddengcn/go-colortext"
 	"github.com/daviddengcn/go-colortext/fmt"
 	"github.com/emicklei/go-restful"
+	"strings"
+	"time"
 )
 
 const moduleWSCron = "cronService"
@@ -79,8 +81,9 @@ func (c *StructCron) CreateWebservice() *restful.WebService {
 	pWs.Path("/cron").
 		Consumes(restful.MIME_JSON).
 		Produces(restful.MIME_JSON)
-	// routes under "cron" endpoint
+	// routes under "cron" endpoint (API)
 	pWs.Route(pWs.POST("upsert").To(c.upsertTimeCronAPI))
+	pWs.Route(pWs.GET("list").To(c.listTimeCronAPI))
 
 	return pWs
 }
@@ -169,6 +172,77 @@ hour24 (default 0), min (default 0), sec (default 0), timezone (default "+00:00"
 	if err != nil {
 		// just log and continue to serve (sometimes it is a disconnection which could be re-covered)
 		c.logError("upsertTimeCronAPI", err.Error())
+	}
+}
+
+func (c *StructCron) listTimeCronAPI(pReq *restful.Request, pRes *restful.Response) {
+	now := time.Now()
+	tzName, tzOffsetInMin := now.Zone()
+	// can construct a new Time... based on data parts
+	now = time.Date(now.Year(), now.Month(), now.Day(), 11, 30, 00, 00, now.Location())
+
+	// use Parse
+	tFormat := "2006-01-02 15:04:05 MST"
+	// tFormat = "2006-01-02T15:04:05-07:00"
+	utcLoc, err := time.LoadLocation("UTC")
+
+	// TODO: use this logic...
+	// a) format a current-time
+	utcFormatTime := now.Format(tFormat)
+	fmt.Println(utcFormatTime)
+	// b) form back into a Time object
+	utcTime, err := time.Parse(tFormat, utcFormatTime)
+	fmt.Println(utcTime)
+	// c) convert to UTC
+	utcTime = utcTime.In(utcLoc)
+	fmt.Println(utcTime)
+
+	// TODO: try another approach (every sec parse the targeted cronTime... not efficient)
+	fmt.Println("")
+	gmtTimeObj, err := time.Parse("2006-01-02T15:04:05-0700", "2019-05-19T15:30:00+0800")
+	tzName, _ = gmtTimeObj.Zone()
+	fmt.Println(gmtTimeObj)
+	//fmt.Println(tzName)
+	//fmt.Println(gmtTimeObj.Local())
+
+	utcTimeObj := gmtTimeObj.In(utcLoc)
+	tzName, _ = utcTimeObj.Zone()
+	fmt.Println(utcTimeObj)
+	//fmt.Println(tzName)
+	//fmt.Println(utcTimeObj.Local())
+
+	// TODO: another approach (1. create cronTime in UTC format {string} - conversion, 2. convert now to utc {string}, 3. compare the values)
+	// RECOMMENDED
+	fmt.Println("\nutc string comparison")
+	fmt.Println("a) target cronTime converted to UTC {string} once : need to update every midnight")
+	// create everyday just after midnight... etc
+	gmtTimeObj, err = time.Parse("2006-01-02T15:04:05-0700", "2019-05-28T15:30:00+0800")
+	strGmtTime := gmtTimeObj.In(time.UTC).Format("2006-01-02T15:04:05-0700")
+	fmt.Println("a => ", strGmtTime)
+	fmt.Println("b) convert current time to utc as well")
+	strUtcTime := time.Now().In(time.UTC).Format("2006-01-02T15:04:05-0700")
+	fmt.Println("b => ", strUtcTime)
+	fmt.Println("c) compare timestamp")
+	fmt.Println(strings.Compare(strGmtTime, strUtcTime))
+
+
+
+
+
+
+
+
+
+
+	//utcTime, err := time.ParseInLocation(tFormat, "2019-05-14T15:30:00+08:00", utcLoc)
+	//utcTime = now.In(utcLoc)
+
+
+// TODO do xxx to UTC conversion here
+	err = pRes.WriteAsJson(fmt.Sprintf("current time: %v, timezone: %v, diff in min: %v; utc time => %v\n", now, tzName, tzOffsetInMin, utcTime))
+	if err != nil {
+		// just log and continue to serve (sometimes it is a disconnection which could be re-covered)
+		c.logError("listTimeCronAPI", err.Error())
 	}
 }
 
